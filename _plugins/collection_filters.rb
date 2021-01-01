d90eab853884a007018ae3974f7c900f1dddcaa3
Jordan Cole <33663+stilist@users.noreply.github.com>
2020-12-31 19:46:07 -0800

    Remove unused method

--- _plugins/collection_filters.rb
+++ _plugins/collection_filters.rb
@@ -10,10 +10,6 @@ module Jekyll
     def collection_entry(key, collection)
       data_collection_record(collection, key)
     end
-
-    def collection_entries(collection)
-      data_collection_records(collection)
-    end
   end
 end
 Liquid::Template.register_filter(Jekyll::CollectionFilters)
